d5a855d95efb9b50bf0d88ebd51419320b20ecb3
isiagi <isiagigeofrey0@gmail.com>
2022-11-03 11:03:05 +0300

    adds fetching

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -3,30 +3,36 @@ import "./home.css";
 
 const Home = () => {
   const [city, setCity] = useState("kampala");
-  const [result, setResult] = useState({});
+  const [result, setResult] = useState(null);
 
   const fetchApi = async (city) => {
     const data = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=`
-    );
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=19b3f1f33b2e5574d503f185c06659f3`
+    ).then((res) => res.json())
+    .then((data) => setResult(data));
 
-    const response = await data.json();
-
-    // console.log(response);
-
-    return response;
+   
   };
 
   useEffect(() => {
     fetchApi(city);
+    console.log("heyy");
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = await fetchApi(city);
-
-    console.log(data);
+    await fetchApi(city);
   };
+
+  // console.log(result.clouds.all);
+
+  // const { clouds, main, name, rain, weather, wind } = result;
+
+  if(result === null) {
+    return <p>Loading</p>
+  }
+
+  console.log(result);
 
   return (
     <div className="home__container">
@@ -45,14 +51,48 @@ const Home = () => {
         </div>
       </div>
       <div className="home__details">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            className=""
-            onChange={(e) => setCity(e.target.value)}
-          />
-          <button type="submit">Search</button>
-        </form>
+        <div className="weather__div">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="home__input"
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <button type="submit" className="weather__button">
+              Search
+            </button>
+          </form>
+          <div className="weather__ul">
+            <ul>
+              <li>Kampala</li>
+              <li>Nairobi</li>
+              <li>Dodoma</li>
+            </ul>
+          </div>
+          <hr />
+        </div>
+        <div className="weather__div">
+          <h4 className="h4">Weather Conditions</h4>
+          <div>
+            <span className="weather__details">
+              <h4>Cloudy</h4>
+              <h3>{result.clouds.all}%</h3>
+            </span>
+            <span className="weather__details">
+              <h4>Humidity</h4>
+              {/* <h3>{result.main.humidity}%</h3> */}
+            </span>
+            <span className="weather__details">
+              <h4>Wind</h4>
+              {/* <h3>{wind.speed}</h3> */}
+            </span>
+            <span className="weather__details">
+              <h4>Rain</h4>
+              <h3>23%</h3>
+            </span>
+          </div>
+          <hr />
+        </div>
       </div>
     </div>
   );
